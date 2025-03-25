ee65fdaf4800c75c7869a8dc350fbe8f77666f06
yagogarmar <yagogarmar@campus.monlau.com>
2025-03-25 18:59:49 +0100

    Modelos creados

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Passport\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
